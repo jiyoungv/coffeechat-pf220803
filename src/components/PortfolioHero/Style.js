ceb8ef896c001367c08b222122af9b4656528d13
박지영 <jyp@vaiv.kr>
2022-08-03 22:58:41 +0900

    Add: PortfolioHero

--- src/components/PortfolioHero/Style.js
+++ src/components/PortfolioHero/Style.js
@@ -1,7 +1,49 @@
 import styled from 'styled-components';
+import { gutter } from '../../styles/Variable';
+import hero_img from '../../assets/images/hero_img.png';
 
 const PortfolioHeros = styled.article`
+    padding-top: 32px;
 
+    .hero-title {
+        h1 {
+            margin-bottom: 8px;
+            font-size: 24px;
+            font-weight: 700;
+            letter-spacing: -0.03em;
+        }
+
+        p {
+            margin-bottom: 8px;
+
+            &:last-of-type {
+                margin-bottom: 0;
+            } 
+        }
+    }
+
+    .hero-img {
+        position: relative;
+        left: -${gutter};
+        width: calc(100% + ${gutter} * 2);
+        height: 276px;
+        margin-top: 10px;
+        background: url(${hero_img}) right center/375px no-repeat;
+    }
+
+    .hero-coupon {
+        padding: 24px 0 40px;
+
+        h2 {
+            margin-bottom: 8px;
+            font-size: 16px;
+            font-weight: 700;
+        }
+
+        figure {
+            
+        }
+    }
 `;
 
 export default PortfolioHeros;
--- src/components/PortfolioHero/index.jsx
+++ src/components/PortfolioHero/index.jsx
@@ -1,9 +1,31 @@
 import PortfolioHeros from './Style';
+import hero_coupon from '../../assets/images/hero_coupon.png';
 
 function PortfolioHero() {
     return (
         <PortfolioHeros>
-            히어로
+            <div className='hero-title'>
+                <h1>
+                    1:1 포트폴리오 리뷰 커피챗 <br/>
+                    5개 분야 OPEN!
+                </h1>
+                <p>드디어 커피챗이 포트폴리오 리뷰를 오픈했습니다.</p>
+                <p>
+                    디자인(UXUI, BX) · 서비스기획 · 마케팅 등 <br/>
+                    총 5개 분야, 약 50명의 파트너가 모였습니다.
+                </p>
+                <p>
+                    내가 궁금한 기업과 직무의 실제 현직자들에게 <br/>
+                    1:1 커피챗으로 나의 포트폴리오 리뷰를 받아 보세요.                    
+                </p>
+            </div>
+            <figure className='hero-img'></figure>
+            <div className='hero-coupon'>
+                <h2>이벤트 기간동안 전원 10% 할인 쿠폰 증정!</h2>
+                <figure>
+                    <img src={hero_coupon} alt='포트폴리오 커피챗 1회(40분) 55,000원 → 49,500원' />
+                </figure>
+            </div>
         </PortfolioHeros>
     );
 }
